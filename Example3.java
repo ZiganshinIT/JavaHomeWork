2d2878787a87d022f00961860366745c01f6a17f
ZiganshinIT <it.ziganshin@gmail.com>
2023-02-07 21:17:54 +0300

    Add Ex3, Ex4, Ex9.

--- Example3.java
+++ Example3.java
@@ -2,13 +2,31 @@ import java.util.Scanner;
 
 public class Example3 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            int num = scanner.nextInt();
-            if (num == 0) {
+        System.out.println("Enter");
+        Scanner sc = new Scanner(System.in);
+        int n = 1, x = 0;
+        x = sc.nextInt(); // 1
+        while(x != 0) { // 1 != 0
+            n = sc.nextInt(); // 2
+            if (x > n) { // 1 > 2
+                x = n;		// comparison 2
+                System.out.println("1st while " + x);
+                break;
+
+            }
+            x = n; // 2
+        }
+        while(x != 0) {
+            n = sc.nextInt(); // 3
+            if (x < n && n != 0) { // 2 < 3 3 < 2
+                System.out.println("Error");
                 break;
             }
-
+            System.out.println("2st while " + x);
+            x = n;
+        }
+        if (n == 0) {
+            System.out.println("Right");
         }
     }
 }
--- Example4.java
+++ Example4.java
@@ -2,20 +2,25 @@ import java.util.Scanner;
 
 public class Example4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int max = 0;
-        int sum = 0;
-        while (true) {
-            int num = scanner.nextInt();
-            if (num == 0) {
-                break;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();	// 1				// input n
+        int max = n, min = n;		// 1 1			// n is assigned max and min
+        int cm = 0, res = 0;
+        n = sc.nextInt(); // 2
+        while (n != 0) {
+            if (n > max) { // 2 > 1
+                max = n;	// 2			// n is assigned max
+                cm = 0;
+                n = sc.nextInt();
+                continue;
             }
-            if (num > max) {
-                max = num;
-                sum = 0;
+            else if (n < min) {
+                min = n;				// n is assigned min
+                res = cm;
             }
-            sum += num;
+            cm += n;
+            n = sc.nextInt();
         }
-        System.out.println(sum);
+        System.out.println("\n" + res);
     }
 }
